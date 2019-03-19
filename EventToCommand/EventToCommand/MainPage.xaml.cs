b16aabe8fe5a9b1e27fbb68e7dc26067b68e4b7d
egbakou <egbakoukodjolaurent@gmail.com>
2019-03-19 12:20:21 +0000

    Behavior created

---
+++ EventToCommand/EventToCommand/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using EventToCommand.ViewModels;
+using Xamarin.Forms;
+
+namespace EventToCommand
+{
+    public partial class MainPage : ContentPage
+    {
+        private MainViewModel viewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new MainViewModel();
+            RegisterMesssages();
+        }
+
+        private void RegisterMesssages()
+        {
+            MessagingCenter.Subscribe<MainViewModel>(this, "Focused", (m) =>
+            {
+                if (m != null)
+                {
+                    DisplayAlert("Bindable Event", "Entry Focused.", "OK");
+                }
+            });
+
+            MessagingCenter.Subscribe<MainViewModel>(this, "TextChanged", (m) =>
+            {
+                if (m != null)
+                {
+                    DisplayAlert("Bindable Event", "Text Changed.", "OK");
+                }
+            });
+        }
+
+
+    }
+}
---
+++ EventToCommand/EventToCommand/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace EventToCommand.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public ICommand EntryFocusedCommand { get; private set; }
+        public ICommand EntryTextChangedCommand { get; private set; }
+
+        public MainViewModel()
+        {
+            EntryFocusedCommand = new Command(FocusedAlert);
+            EntryTextChangedCommand = new Command(TextChangedAlert);
+        }
+
+
+        private void FocusedAlert()
+        {
+            MessagingCenter.Send(this, "Focused");
+        }
+
+        private void TextChangedAlert()
+        {
+            MessagingCenter.Send(this, "TextChanged");
+        }
+    }
+}
